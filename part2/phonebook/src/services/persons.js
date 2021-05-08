363189a627a31775d09c383449c66a1daf6a7a53
Johnny Wagner <github@johnnybu.com>
2021-05-08 21:03:07 +0000

    changes to service for backend app on heroku

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:3001/persons';
+const baseUrl = '/api/persons';
 
 const createPerson = async person => {
     const response = await axios.post(baseUrl, person);
